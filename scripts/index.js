1ece32305197d9e25d59302c5a129fd07d031059
Dmitriy Belozerov <dm.belozerov@yandex.ru>
2022-04-19 22:06:36 +0300

    проверить3

--- scripts/index.js
+++ scripts/index.js
@@ -2,30 +2,25 @@ const profileEditButton = document.querySelector('.profile__edit');
 const formClose = document.querySelector('.form__close');
 const editForm = document.querySelector('.popup');
 
-function formEditToggle() {
-    
-    editForm.classList.toggle('popup_opened');
+function formEditOpen() {
+    editForm.classList.add('popup_opened');
     formNameElement.value = nameProfile.textContent;
     formJobNameElement.value = aboutProfile.textContent;
 }
 
-profileEditButton.addEventListener('click', formEditToggle);
-formClose.addEventListener('click', formEditToggle);
+profileEditButton.addEventListener('click', formEditOpen);
 
+function formEditClose() {
+    editForm.classList.remove('popup_opened');
+}
 
-
+formClose.addEventListener('click', formEditClose);
 
 let formNameElement = document.querySelector('.form__input_type_name');
-
 let formJobNameElement = document.querySelector('.form__input_type_about');
-
 let formSubmit = document.querySelector('.form');
-console.log(formSubmit);
-
 let nameProfile = document.querySelector('.profile__name');
-
 let aboutProfile = document.querySelector('.profile__about-name');
-
 
 function formSubmitHandler() {
     nameProfile.textContent = formNameElement.value;
