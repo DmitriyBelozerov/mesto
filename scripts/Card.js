2946d1992e5bdf1572f7dc59c2b8aad0be02b3a5
Dmitriy Belozerov <dm.belozerov@yandex.ru>
2022-09-23 22:34:08 +0300

    refactoring form validator 23/09/22

--- scripts/Card.js
+++ scripts/Card.js
@@ -4,6 +4,13 @@ export class Card {
         this._link = data.link;
         this._openPopup = openPopup;
         this._popupViewPhoto = document.querySelector('.popup_view_photo');
+        this._containerViewPhotoName = document.querySelector('.container-view-photo__photo');
+        this._containerViewPhotoTitle = document.querySelector('.container-view-photo__title');
+        this._element = this._getTemplate();
+        this._cardDelete = this._element.querySelector('.card__delete');
+        this._cardLike = this._element.querySelector('.card__like');
+        this._cardPhoto = this._element.querySelector('.card__photo');
+        this._cardTitle = this._element.querySelector('.card__title');
     }
 
     _getTemplate() {
@@ -12,41 +19,37 @@ export class Card {
             .content
             .querySelector('.card')
             .cloneNode(true);
-
         return cardElement;
     }
-    _addEventListeners() {
-        this._element.querySelector('.card__delete').
+    _addEventListeners = () => {
+        this._cardDelete.
             addEventListener('click', this._deleteTask);
-
-        this._element.querySelector('.card__like').
+        this._cardLike.
             addEventListener('click', this._likeTask);
-
-        this._element.querySelector('.card__photo').
+        this._cardPhoto.
             addEventListener('click', this._openPreview);
     }
+
     _deleteTask = (event) => {
         event.preventDefault();
         this._element.remove();
     }
+
     _likeTask = (event) => {
         event.target.classList.toggle("card__like_active");
     }
+
     _openPreview = () => {
-        const cardPhoto = this._element.querySelector('.card__photo');
-        const cardTitle = this._element.querySelector('.card__title');
-        const containerViewPhotoPhoto = document.querySelector('.container-view-photo__photo');
-        const containerViewPhotoTitle = document.querySelector('.container-view-photo__title');
-        containerViewPhotoPhoto.src = cardPhoto.src;
-        containerViewPhotoPhoto.alt = cardTitle.textContent;
-        containerViewPhotoTitle.textContent = cardTitle.textContent;
+                this._containerViewPhotoName.src = this._cardPhoto.src;
+        this._containerViewPhotoName.alt = this._cardTitle.textContent;
+        this._containerViewPhotoTitle.textContent = this._cardTitle.textContent;
         this._openPopup(this._popupViewPhoto);
     }
 
     generateCard() {
-        this._element = this._getTemplate();
-        this._element.querySelector('.card__title').textContent = this._name;
-        this._element.querySelector('.card__photo').src = this._link;
+        this._cardTitle.textContent = this._name;
+        this._cardPhoto.alt = this._name;
+        this._cardPhoto.src = this._link;
         this._addEventListeners();
         return this._element;
     }
